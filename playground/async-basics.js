223729793d9b61941d3c5fb6fb3fcb940ebf22ab
Gustavo Ramírez <gustavo81988@gmail.com>
2016-12-17 15:12:43 -0400

    Async Basics

---
+++ playground/async-basics.js
@@ -0,0 +1,12 @@
+console.log('Starting app');
+
+setTimeout(()=>{
+    console.log('Inside callback');
+},2000);
+
+setTimeout(()=>{
+    console.log('Second setTimeout');
+},0);
+
+
+console.log('Finishing up');
