84a72bbe22c79517a063a4623bdfd4fa082af0cd
gustavo <gustavo81988@gmail.com>
2016-12-23 19:53:44 -0430

    Pretty Printing Objects

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request( {
     url:'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCxKCa8d27gDfpLPO8pjjKmyzj9xLX1WTc',
     json: true
 },(error, response, body)=> {
-    console.log(body);
-})
+    console.log(JSON.stringify(body, undefined, 2));
+});
