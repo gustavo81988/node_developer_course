2981d31524b315e931e81565ed7ab086655d6097
gustavo <gustavo81988@gmail.com>
2016-12-23 23:33:09 -0430

    Callback Errors

--- app.js
+++ app.js
@@ -14,13 +14,19 @@ const argv    = yargs
     .alias('help','h')
     .argv;  
 
-    let encodedAddress = encodeURIComponent(argv.address);
+let encodedAddress = encodeURIComponent(argv.address);
     
 request( {
     url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCxKCa8d27gDfpLPO8pjjKmyzj9xLX1WTc`,
     json: true
 },(error, response, body)=> {
-    console.log(`Adress:   ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if(error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS'){
+        console.log('Unable to find that address');
+    } else if (body.status === 'OK') {
+        console.log(`Adress:   ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
