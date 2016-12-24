7b21fb04c74b546db27a522b0c214d02be064dec
gustavo <gustavo81988@gmail.com>
2016-12-23 23:10:37 -0430

    Encoding User Input

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs   = require('yargs');
 
+const argv    = yargs
+    .options({
+        a:{
+            demand   : true,
+            alias    : 'address',
+            describe : 'Address to fetch weather for',
+            string   : true    
+        }    
+    })
+    .help()
+    .alias('help','h')
+    .argv;  
+
+    let encodedAddress = encodeURIComponent(argv.address);
+    
 request( {
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCxKCa8d27gDfpLPO8pjjKmyzj9xLX1WTc',
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCxKCa8d27gDfpLPO8pjjKmyzj9xLX1WTc`,
     json: true
 },(error, response, body)=> {
     console.log(`Adress:   ${body.results[0].formatted_address}`);
