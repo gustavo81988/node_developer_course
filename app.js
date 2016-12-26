d84aad392db0020adea8d0fbab849f8eb917c091
gustavo <gustavo81988@gmail.com>
2016-12-25 20:58:03 -0430

    Wiring Up Weather Search

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yargs   = require('yargs');
+/*const yargs   = require('yargs');
 const geocode = require('./geocode/geocode'); 
 
 const argv    = yargs
@@ -19,5 +19,22 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
         console.log(errorMessage);    
     } else {
         console.log(JSON.stringify(results, undefined, 2));
+    }
+});
+
+*/
+
+const  request = require('request');
+
+request({
+    url:'https://api.darksky.net/forecast/b0efcb4fbeb3f2696692bbdf6a2df57d/37.8267,-122.4233',
+    json: true
+},(error,response,body) =>{
+    if(error){
+        console.log('Unable to conect to Forecast.io server');
+    }else if(response.statusCode === 400){
+        console.log('Unable to fetch weather'); 
+    }else if(response.statusCode === 200){
+        console.log(body.currently.temperature); 
     }
 });
