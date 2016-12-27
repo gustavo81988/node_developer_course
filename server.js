6eb70a96017bcb99ddd97e59a5dc1b88bce2ebff
gustavo <gustavo81988@gmail.com>
2016-12-27 11:48:57 -0430

    Advanced Templating

--- server.js
+++ server.js
@@ -2,21 +2,29 @@ const express = require('express');
 const hbs     = require('hbs');
 let app       = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express!!</h1>');
     res.render('home.hbs',{
         pageTitle   : 'Home Page',
         welcomeMessage : 'Welcome to my website',
-        currentYear : new Date().getFullYear()
     });
 });
 
 app.get('/about',(req , res) => {
     res.render('about.hbs',{
         pageTitle   : 'About Page',
-        currentYear : new Date().getFullYear()
     });
 });
 
